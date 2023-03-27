33b8bca72ebf0225c12775be5fdc880485b55fcc
wilsterdc <delacruz.wilster15@gmail.com>
2023-03-27 22:57:34 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv/config')
 const { Client, IntentsBitField } = require('discord.js');
-const { Configuration } = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
 
 const client = new Client({
     intents: [
@@ -14,8 +14,43 @@ client.on('ready', () => {
     console.log('The bot is online.');
 });
 
-client.on('messageCreate', (message) => {
-    console.log(message);
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    
+    let conversationLog = [{role: 'system', content: 'Dr. Vegapunk AI'}];
+
+    try {
+        await message.channel.sendTyping();
+
+        let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+        prevMessages.reverse();
+
+        prevMessages.forEach((msg) => {
+            conversationLog.push({
+                role: 'user',
+                content: msg.content,
+            });
+        });
+
+        const result = await openai
+        .createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: conversationLog,
+        })
+        .catch((error) => {
+            console.log(`OPENAI ERR: ${error}`);
+        });
+        
+        message.reply(result.data.choices[0].message);
+    } catch (error) {
+        console.log(`ERR: ${error}`);
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
